907b764c6dbab0c7c246c5a3c7a8e241f7407639
Ahsan Naveed <anaveed@sfu.ca>
2018-02-06 21:41:03 -0500

    MongoDB setup and connection

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+var db;
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const MongoClient = require("mongodb").MongoClient;
@@ -5,9 +7,17 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen("3000", () => {
-  console.log("listening on port 3000");
-});
+// db setup
+MongoClient.connect(
+  "mongodb://starWarior:devTest123@ds125588.mlab.com:25588/star-wars-quotes",
+  (err, client) => {
+    if (err) return console.log(err);
+    db = client.db("star-wars-quotes");
+    app.listen("3000", () => {
+      console.log("listening on port 3000");
+    });
+  }
+);
 
 // serve index.html from root dir
 app.get("/", (req, res) => {
